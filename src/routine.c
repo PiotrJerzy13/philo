7224c02fbc9c24b5f5d6a874540603e01b574606
Piotr Wojnarowski <pwojnaro@1-A-31.42heilbronn.de>
2024-10-08 12:10:20 +0200

    Fix printing output

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 15:17:16 by pwojnaro          #+#    #+#             */
-/*   Updated: 2024/10/03 19:37:17 by pwojnaro         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:07:53 by pwojnaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	philo_eat(t_philo *philo)
 	if (death_mutex_check(philo) == 1)
 		return ;
 	pthread_mutex_lock(philo->left_fork);
-	print_mutex_lock(philo, "has taken the left fork");
+	print_mutex_lock(philo, "has taken a fork");
 	pthread_mutex_lock(philo->right_fork);
-	print_mutex_lock(philo, "has taken the right fork");
+	print_mutex_lock(philo, "has taken a fork");
 	set_meal_time(philo);
 	print_mutex_lock(philo, "is eating");
 	ft_usleep(philo->time_to_eat);
@@ -53,7 +53,10 @@ void	*philosopher_routine(void *arg)
 	if (philo->data->num_philo == 1)
 		return (handle_one_philo(philo));
 	if (philo->id % 2 != 0)
+	{
+		print_log(philo->id, "is thinking", philo->time.start_time);
 		ft_usleep(50);
+	}
 	while (death_mutex_check(philo) == 0)
 	{
 		if (philo->data->num_philo % 2 == 0)
--- src/time.c
+++ src/time.c
@@ -6,7 +6,7 @@
 /*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 16:13:44 by pwojnaro          #+#    #+#             */
-/*   Updated: 2024/10/03 19:38:11 by pwojnaro         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:05:00 by pwojnaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ uint64_t	get_current_time_ms(void)
 
 	if (gettimeofday(&time, NULL) != 0)
 	{
-		perror("gettimeofday failed");
 		return (0);
 	}
 	current_time = (uint64_t)(time.tv_sec * 1000) + (time.tv_usec / 1000);
